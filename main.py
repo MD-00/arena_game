985ace1c858c637c89d0fdc25281f377895d7b6e
ARCTIC <dybek1.9@gmail.com>
2021-05-06 16:19:34 +0200

    NEAT Implemented

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def blitRotateCenter(surf, image, topleft, angle):
     surf.blit(rotated_image, new_rect.topleft)
 
 
-def draw_window(window, bird, pipes, score):
+def draw_window(window, birds, pipes, score):
     window.blit(BG_IMG, (0, 0))
 
     for pipe in pipes:
@@ -104,13 +104,15 @@ def draw_window(window, bird, pipes, score):
 
     text = font.render("Score: " + str(score), 1, (255, 255, 255))
     window.blit(text, (setup.width - 20 - text.get_width(), 10))
-    bird.draw(window)
+
+    for bird in birds:
+        bird.draw(window)
     pg.display.update()
 
 
 class Pipe:
-    gap = 225
-    vel = 5
+    gap = 177
+    vel = 9
 
     def __init__(self, x):
         self.x = x
@@ -154,7 +156,18 @@ class Pipe:
 
 
 def fitness_fun(genomes, config):
-    bird = Bird(230, 350)
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+
     pipes = [Pipe(700)]
 
     score = 0
@@ -167,41 +180,76 @@ def fitness_fun(genomes, config):
         for event in pg.event.get():
             if event.type == pg.QUIT or keys_pressed[pg.K_ESCAPE]:
                 gameOn = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    bird.jump()
-        draw_window(screen, bird, pipes, score)
+                pg.quit()
+                quit()
+            # if event.type == pg.KEYDOWN:
+            #     if event.key == pg.K_w:
+            #         bird.jump()
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].pipe_top.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+            # ^^ tą wartość trzeba zmienić
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+            if output[0] > 0.45:
+                bird.jump()
+
+
 
         add_pipe = False
         rem = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
-                # gameOn = False
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    # birds.remove(bird)
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
 
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
             if pipe.x + pipe.pipe_top.get_width() < 0:
                 rem.append(pipe)
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
+
 
             pipe.move()
+
         if add_pipe:
             score += 1
+            for g in ge:
+                # można dać 10 fitnessu
+                g.fitness += 10
+
             pipes.append(Pipe(600))
+
         for r in rem:
             pipes.remove(r)
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= setup.height or bird.y < 0:
 
-        if bird.y + bird.img.get_height() >= setup.height:
-            pass
-            # gameOn=False
-        bird.move()
+                # w razie czego wywalić ten fitness
+                ge[x].fitness -= 1
+                # birds.remove(bird)
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
-    pg.quit()
-    quit()
 
 
-fitness_fun()
+        draw_window(screen, birds, pipes, score)
+
+
 
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
@@ -211,7 +259,7 @@ def run(config_path):
     stats = neat.StatisticsReporter()
     population.add_reporter(stats)
 
-    winner = population.run(fitness_fun, 50)
+    winner = population.run(fitness_fun, 200)
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
